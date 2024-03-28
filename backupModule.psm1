e62c6959dd0f08ed21ba36b6d5fb1b81304d014b
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-03-28 18:04:58 +0700

    :recycle: rft: rewrite a better backup-Extensive

--- backupModule.psm1
+++ backupModule.psm1
@@ -5,13 +5,17 @@ $backUpDir = @{
 
 
 
-function Backup-Extensive()
+function Backup-Extensive($Verbose = $null)
 {
 
   foreach ($startDir in $backUpDir.Keys)
   {
     $destination = $($backUpDir.$startDir)
     Copy-Item -Path $startDir -Destination $destination -Force -Recurse
+    if($Verbose -le 0)
+    {
+      $startDir = Format-LimitLength $startDir
+    }
     Write-Host "$startDir backed up" -ForegroundColor Cyan 
   }
 }
