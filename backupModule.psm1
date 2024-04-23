b54993ff512f1d8c8dbb8dd4261d3eda6c0b12fc
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-04-23 22:29:00 +0700

    :bug: fix: kicad wrong position.

--- backupModule.psm1
+++ backupModule.psm1
@@ -1,5 +1,5 @@
 $backUpDir = @{
-  "$env:kicadSettingDir" = "$env:dotfilesRepo\.config" 
+  "$env:kicadSettingDir" = "$env:dotfilesRepo\.config\.kicad" 
   "$HOME\PenTablet_Config.pcfg" = "$env:dotfilesRepo\.config\PenTablet_Config.pcfg"
   "$env:APPDATA\copyq" = "$env:dotfilesRepo\.config\"
   "$HOME\.gitconfig" = "$env:dotfilesRepo\"
