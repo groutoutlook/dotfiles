7be0703da62ad6d8bd9e52b262ef068fbb1782a7
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2024-05-16 17:38:45 +0700

    :camera_flash: add decent mousemaster files

--- backupModule.psm1
+++ backupModule.psm1
@@ -1,7 +1,6 @@
 $backUpDir = @{
   "$env:kicadSettingDir" = "$env:dotfilesRepo\.config\.kicad" 
   "$HOME\PenTablet_Config.pcfg" = "$env:dotfilesRepo\.config\PenTablet_Config.pcfg"
-  "$env:APPDATA\copyq" = "$env:dotfilesRepo\.config\"
   "$HOME\.gitconfig" = "$env:dotfilesRepo\"
   "$HOME\.gitignore_global" = "$env:dotfilesRepo\"
   "$HOME\.config\jrnl" = "$env:dotfilesRepo\.config\"
