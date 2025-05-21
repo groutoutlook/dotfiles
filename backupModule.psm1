0bcd2d39d89547e71252685e6950ed53e6772107
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-05-21 13:25:52 +0700

    wip

--- backupModule.psm1
+++ backupModule.psm1
@@ -4,10 +4,7 @@ $backUpDir = @{
   "$HOME\.gitconfig" = "$env:dotfilesRepo\"
   "$env:LOCALAPPDATA\Packages\Microsoft.WindowsTerminalPreview_*\LocalState\settings.json" = "$env:dotfilesRepo\config\WindowsTerminal"
   "$env:usrbinD\mousemaster.properties" = "$env:dotfilesRepo\config\"
-  "$HOME\Downloads\vimium_c*.json" = "$env:dotfilesRepo\config\vimium_c_edge.json"
 }
-
-
 function Backup-Extensive($Verbose = $null)
 {
   foreach ($startDir in $backUpDir.Keys)
@@ -19,9 +16,8 @@ function Backup-Extensive($Verbose = $null)
     
     if($Verbose -le 0)
     {
-      $startDir = Format-LimitLength $startDir
+      $startDir = $startDir
     }
     Write-Host "$startDir backed up" -ForegroundColor Cyan 
   }
 }
-Set-Alias -Name backupdot -Value Backup-Extensive
--- justfile
+++ justfile
@@ -7,11 +7,11 @@ set unstable
 # set dotenv-filename	:= ".env"
 # set dotenv-required := true
 _default:
-    @just --choose
+    @just --list
 
 alias r := run
 default_args := 'args here'
-run *args: _clean_old_ptoy
+run *args: _clean_old_ptoy && _fetch_new_vimium
     :bak && git rap 
 
 [script]
@@ -19,7 +19,12 @@ _clean_old_ptoy:
     gci settings_* -Recurse -Depth 1 | sort -property LastWriteTime `
     | select -skiplast 1 | rm -Recurse -Force
     
-
+[script]
+_fetch_new_vimium:
+    gci -Path ~/Downloads -Filter vimium_c* -Recurse -Depth 1 | sort -property LastWriteTime `
+    | select -skiplast 1 | rm -Recurse -Force
+    cp ~/Downloads/vimium_c* ./config/vimium_c_edge.json
+    
 alias b := build
 build:
     :bak && git cif
