70af446768faed3ec26d3ac78a90b43762d3e0f8
groutoutlook <150989799+groutoutlook@users.noreply.github.com>
2025-07-13 12:20:53 +0700

    chore: better theme for helix.

---
+++ config/helix/init.scm
@@ -0,0 +1,14 @@
+(require "helix/configuration.scm")
+
+(require "cogs/keymaps.scm")
+
+; Scooter
+(require "../../Development/scooter-hx/scooter.hx/scooter.scm")
+(keymap (global) (normal (space (q ":scooter"))))
+
+; Smooth scroll
+(require "../../Development/smooth-scroll.hx/smooth-scroll.scm")
+(keymap (global) (normal (C-d half-page-down-smooth)))
+(keymap (global) (normal (C-u half-page-up-smooth)))
+(keymap (global) (normal (pageup page-up-smooth)))
+(keymap (global) (normal (pagedown page-down-smooth)))
